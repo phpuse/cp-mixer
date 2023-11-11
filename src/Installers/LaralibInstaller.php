2345e161505fd45734203a189d32bbea42532790
Carl <2009wpeng@gmail.com>
2023-11-11 22:44:15 +0800

    carl push

--- src/Installers/LaralibInstaller.php
+++ src/Installers/LaralibInstaller.php
@@ -6,8 +6,8 @@ class LaralibInstaller extends BaseInstaller
 {
     /** @var array<string, string> */
     protected array $locations = [
+        'lada' => 'lada/{$name}/',
         'lamo' => 'lamo/{$name}/',
-        'lause' => 'lause/{$name}/',
         'layer' => 'layer/{$name}/',
     ];
 }
